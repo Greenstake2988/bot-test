61f9adbffccc2906e30b0b2f8a8043b633343b25
Oscar <0skr.vazquez@gmail.com>
2021-05-22 19:13:12 -0500

    agreagamos alta en anymsg

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ int main() {
 			//Checamos que el nombre del cliente no este vacio.
 			if(not clientes_guardados[std::to_string(message->from->id)]["nombre"].empty()) {
 				bot.getApi().sendMessage(message->chat->id, "Hola " + clientes_guardados[std::to_string(message->from->id)]["nombre"].get<std::string>() + " que deseas ordenar:\n"+
-												            "Maiz                     Precio\n"
+												            "Maiz                                Precio\n"
 															"Tacos de asado         $13 /tma\n"+
 															"Tacos con chicharra $14 /tmc\n"+
 															"Especiales                  $15 /tme\n"
