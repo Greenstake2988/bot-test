313c73b40000ca2c47a7ae59fa19e896a6cbc22e
Oscar <0skr.vazquez@gmail.com>
2021-05-23 14:25:46 -0500

    agreagamos alta en anymsg

--- src/main.cpp
+++ src/main.cpp
@@ -298,7 +298,7 @@ void escribirCliente(string id_cliente, json cliente){
 	//Abrimos el archivo clientes modo escritura
 	ofstream clientes_guardados("clientes.json");
 	json clientes_copia = copiaClientes();
-	clientes_copia.erase[id_cliente];
+	clientes_copia.erase(id_cliente);
 	clientes_copia.[id_cliente] = cliente;
 	clientes_guardados << clientes_copia;
 	clientes_guardados.close();
